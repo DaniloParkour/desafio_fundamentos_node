896f8cc366cff3e889d5164c369b7aa0d51df721
DaniloParkour <danilomlcarvalho@gmail.com>
2020-04-15 22:46:02 -0300

    Salvar Ponto Funcionando - Trocar Vars Por Const

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,52 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    /* const incomeArray = this.transactions.filter(value =>
+      value.type === 'income' ? value : 0,
+    );
+
+    const outcomeArray = this.transactions.filter(value =>
+      value.type === 'outcome' ? value : 0,
+    );
+
+    const incomeValue = incomeArray.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.value,
+    );
+
+    const outcomeValue = outcomeArray.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.value,
+    ); */
+
+    const sumIncome = 0;
+    const sumOutcome = 0;
+
+    // | 'outcome'
+
+    this.transactions.map(transaction => {
+      if (transaction.type === 'income') sumIncome += transaction.value;
+      else sumOutcome += transaction.value;
+    });
+
+    const balance: Balance = {
+      /* income: incomeValue,
+      outcome: outcomeValue,
+      total: incomeValue - outcomeValue, */
+      income: sumIncome,
+      outcome: sumOutcome,
+      total: sumIncome - sumOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
+const transactions = new TransactionsRepository();
+
 // const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const allTransactions = transactions.all();
+    const balance = transactions.getBalance();
+    response.json({ transactions: allTransactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +23,20 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    // TODO => title, value e type(income/outcome)
+    /* {
+      "i  d": "uuid",
+      "title": "Salário",
+      "value": 3000,
+      "type": "income"
+    } */
+
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(transactions);
+
+    const transaction = createTransaction.execute({ title, value, type });
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
